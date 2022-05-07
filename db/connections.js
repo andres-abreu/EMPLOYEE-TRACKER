680e4a93bf4e8d12e2e4aece0f27ae3f67b1bb66
Andres: Abreu <andresabreu@andress-MacBook-Pro.local>
2022-05-06 21:24:09 -0400

    code-fix, sql-fix

--- db/connections.js
+++ db/connections.js
@@ -3,7 +3,7 @@ const mysql = require('mysql2');
 const db = mysql.createConnection({
     host: 'localhost',
     user: 'root',
-    password: 'fr4nc3ll4',
+    password: 'amitayus',
     database: 'employees',
 },
 console.log('Connected to the employees database.')
